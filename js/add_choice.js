4615c296b3fa146e8ebdee42e07a6747938878c1
Tyler-A <Tyler.akins1999@gmail.com>
2019-09-10 01:00:05 -0600

    The beginnings.

---
+++ js/add_choice.js
@@ -0,0 +1,10 @@
+const add_choice = () => {
+    let index = document.getElementsByClassName("choice").length;
+
+
+    let choice_div = document.getElementById("choice-template");
+    let clone = choice_div.content.cloneNode(true);
+    clone.childNodes[1].childNodes[1].id = `text-in-${index}`;
+    clone.childNodes[1].childNodes[3].id = `weight-in-${index}`;
+    document.getElementById("choices").appendChild(clone);
+}
