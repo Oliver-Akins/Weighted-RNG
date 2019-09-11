2776e10f80c0763923abab3531e1bdc91aeb466f
Tyler-A <Tyler.akins1999@gmail.com>
2019-09-10 18:53:14 -0600

    Change from .childNodes to .children

--- js/add_choice.js
+++ js/add_choice.js
@@ -4,8 +4,8 @@ const add_choice = () => {
 
     let choice_div = document.getElementById("choice-template");
     let clone = choice_div.content.cloneNode(true);
-    clone.childNodes[1].childNodes[1].id = `text-in-${index}`;
-    clone.childNodes[1].childNodes[3].id = `weight-in-${index}`;
+    clone.children[0].children[0].id = `text-in-${index}`;
+    clone.children[0].children[1].id = `weight-in-${index}`;
 
     // Add to document without resetting the other ones
     document.getElementById("choices").appendChild(clone);
